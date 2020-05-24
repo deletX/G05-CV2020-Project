0f7c440ee9952aa5d12f702ddd299871776de29d
marco <marcogambelli96@gmail.com>
2020-05-24 20:33:44 +0200

    Aggiornato metodo painting detection

--- preprocessing/camera_calibration/painting_detection.py
+++ preprocessing/camera_calibration/painting_detection.py
@@ -2,18 +2,19 @@ import cv2
 import numpy as np
 
 
-def empty(a):
-    pass
-
-
-def getContours(img, imgContour):
+def get_contours(frame):
+    img_cnts = frame.copy()
+    img_blur = cv2.GaussianBlur(frame, (7, 7), 1)
+    img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+    img_canny = cv2.Canny(img_gray, 23, 20)
+    kernel = np.ones((5, 5))
+    img_dil = cv2.dilate(img_canny, kernel, iterations=1)
+    contours, hierarchy = cv2.findContours(img_dil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     bbox_list = []
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        areaMin = 10000
-        if area > areaMin:
-            #cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
+        area_min = 10000
+        if area > area_min:
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
             print(len(approx))
@@ -21,12 +22,10 @@ def getContours(img, imgContour):
                 x, y, w, h = cv2.boundingRect(approx)
                 bbox = {'x': x, 'y': y, 'h': h, 'w': w}
                 bbox_list.append(bbox)
-                cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
-                cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20),
-                            cv2.FONT_HERSHEY_COMPLEX, .7, (0, 255, 0), 2)
-                cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45),
+                cv2.rectangle(img_cnts, (x, y), (x + w, y + h), (0, 255, 0), 5)
+                cv2.putText(img_cnts, "Area: " + str(int(area)), (x + w + 20, y + 20),
                             cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
-    return bbox_list
+    return bbox_list, img_cnts
 
 
 def main():
@@ -39,14 +38,8 @@ def main():
         ret, frame = cap.read()
         if not ret:
             break
-        imgContour = frame.copy()
-        imgBlur = cv2.GaussianBlur(frame, (7, 7), 1)
-        imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-        imgCanny = cv2.Canny(imgGray, 23, 20)
-        kernel = np.ones((5, 5))
-        imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
-        bbox_list = getContours(imgDil, imgContour)
-        cv2.imshow("Result", imgContour)
+        bbox_list, img_cnts = get_contours(frame)
+        cv2.imshow("Result", img_cnts)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
