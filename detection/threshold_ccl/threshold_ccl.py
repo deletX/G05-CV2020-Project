37d5b5d16b4f9dcbe3c09e2bb5d6993030fdcd6d
zer0meister <nicholas.glorio@gmail.com>
2020-05-31 15:40:39 +0200

    fine tuning threshold_ccl.py

--- detection/threshold_ccl/threshold_ccl.py
+++ detection/threshold_ccl/threshold_ccl.py
@@ -2,21 +2,21 @@ import cv2
 import numpy as np
 import json
 
-def run():
-    bboxes = {}
-
-    for i in range(1, 28):
-        
-        original = cv2.imread("detection\\threshold_ccl\\input\\{0:0=2d}.jpg".format(i),
-                            cv2.IMREAD_UNCHANGED)
-        
-        cv2.imwrite("./output/{0:0=2d}.jpg".format(i), img)
-        bboxes["{0:0=2d}.jpg".format(i)] = img_list
-
-    print(bboxes)
-    with open("bboxes_tccl.json","w") as out:
-        json_obj = json.dumps(bboxes)
-        out.write(json_obj)
+# def run():
+#     bboxes = {}
+#
+#     for i in range(1, 28):
+#
+#         original = cv2.imread("detection\\threshold_ccl\\input\\{0:0=2d}.jpg".format(i),
+#                             cv2.IMREAD_UNCHANGED)
+#
+#         cv2.imwrite("./output/{0:0=2d}.jpg".format(i), img)
+#         bboxes["{0:0=2d}.jpg".format(i)] = img_list
+#
+#     print(bboxes)
+#     with open("bboxes_tccl.json","w") as out:
+#         json_obj = json.dumps(bboxes)
+#         out.write(json_obj)
 
 def run_frame(hsv_frame):
     img_list = []
@@ -25,10 +25,10 @@ def run_frame(hsv_frame):
     # img = cv2.medianBlur(img,5)
     # img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,33,3)
 
-    img = cv2.GaussianBlur(img, (5, 5), 1)
-    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    img = cv2.GaussianBlur(img, (7, 7), 1)
+    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_TRIANGLE)
 
-    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, (5, 5), iterations=3)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, (11, 11), iterations=3)
     retval, img, stats, _ = cv2.connectedComponentsWithStatsWithAlgorithm(img, 8, cv2.CV_16U, cv2.CCL_GRANA)
 
     for j in range(retval):
