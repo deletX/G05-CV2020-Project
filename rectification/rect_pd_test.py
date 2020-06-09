c162cb94d2c3ce235c5d8ede98a544894764ae4f
deletX <37343881+deletX@users.noreply.github.com>
2020-06-09 16:59:08 +0200

    Risolto problema orientazione quadri. Se trovate artefatti è colpa di bbox inconsistenti.

--- rectification/rect_pd_test.py
+++ rectification/rect_pd_test.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 import json
-
+import random
 from detection.canny_hough.painting_detection import get_contours
+from math import sqrt
 
 
 def crop(img, box):
@@ -25,29 +26,37 @@ if __name__ == "__main__":
                               cv2.IMREAD_UNCHANGED)
         np.swapaxes(original, 0, 1)
         bbox_list, bbox_img = get_contours(original)
-        # cv2.imshow("or", cv2.resize(original, (1920, 1080)))
-        # cv2.waitKey()
+
         out = original.copy()
         for bbox in bbox_list:
             copy = original.copy()
+            src_pts = []
+            dst_pts = [
+                [bbox['x'], bbox['y']],
+                [bbox['x'], bbox['y'] + bbox['height']],
+                [bbox['x'] + bbox['width'], bbox['y'] + bbox['height']],
+                [bbox['x'] + bbox['width'], bbox['y']],
+            ]
 
-            dst_pts = np.array(
-                [(bbox['x'], bbox['y']),
-                 (bbox['x'], bbox['y'] + bbox['height']),
-                 (bbox['x'] + bbox['width'], bbox['y'] + bbox['height']),
-                 (bbox['x'] + bbox['width'], bbox['y'])],
-                dtype=np.float32)
-            dst_pts = dst_pts[:, np.newaxis, :]
+            edges = np.squeeze(bbox['approx'])
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            for el in dst_pts:
+                sortd = sorted(edges, key=lambda v: sqrt((el[0] - v[0]) ** 2 + (el[1] - v[1]) ** 2))
+                src_pts.append(sortd[0])
 
-            transform_matrix, _ = np.asarray(cv2.findHomography(bbox['approx'], dst_pts))
+            dst_pts = np.array(dst_pts, dtype=np.int32)
+            src_pts = np.array(src_pts, dtype=np.int32)
+
+            transform_matrix, _ = cv2.findHomography(src_pts, dst_pts)
             warp = cv2.warpPerspective(copy, transform_matrix, dsize=(copy.shape[1], copy.shape[0]))
             cropped = crop(warp, bbox)
+
             out[bbox['y']:bbox['y'] + bbox['height'], bbox['x']:bbox['x'] + bbox['width']] = cropped
             bbox.pop('approx', None)
-        # cv2.imshow("or", cv2.resize(out, (1920, 1080)))
-        # cv2.waitKey()
+
         cv2.imwrite("./output/{0:0=2d}.jpg".format(img_i), out)
         bboxs["{0:0=2d}.jpg".format(img_i)] = bbox_list
+
     with open("rect_bboxs.json", "w") as out:
         print(bboxs)
         json_obj = json.dumps(bboxs)
