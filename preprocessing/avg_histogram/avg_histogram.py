6e10caf7cd6a7d968e578ab16ce831dada6015d7
deletX <37343881+deletX@users.noreply.github.com>
2020-06-16 18:15:44 +0200

    refactor avg_histogram.py (ho usato le variabili anzichè hardcodarle)

--- preprocessing/avg_histogram/avg_histogram.py
+++ preprocessing/avg_histogram/avg_histogram.py
@@ -27,9 +27,9 @@ if __name__ == "__main__":
         histSize = 256
         histRange = (0, 256)  # the upper boundary is exclusive
 
-        hist_b = cv2.calcHist(bgr_planes, [0], None, [256], (0, 256))
-        hist_g = cv2.calcHist(bgr_planes, [1], None, [256], (0, 256))
-        hist_r = cv2.calcHist(bgr_planes, [2], None, [256], (0, 256))
+        hist_b = cv2.calcHist(bgr_planes, [0], None, [histSize], histRange)
+        hist_g = cv2.calcHist(bgr_planes, [1], None, [histSize], histRange)
+        hist_r = cv2.calcHist(bgr_planes, [2], None, [histSize], histRange)
 
         img_b += hist_b
         img_g += hist_g
