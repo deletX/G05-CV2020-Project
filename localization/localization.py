ea7f09c897f42ab5aed07d3ee6fd61a1e6ba4acb
deletX <37343881+deletX@users.noreply.github.com>
2020-06-17 15:15:56 +0200

    Aggiunta la localizzazione (localization.py)

--- localization/localization.py
+++ localization/localization.py
@@ -1,5 +1,6 @@
 def localization(painting_bboxes):
     votes = [0 for i in range(22)]
     for bbox in painting_bboxes:
-        votes[bbox["painting"]["room"]] += 1
-    return votes.index(max(votes))
+        if "painting" in bbox:
+            votes[int(bbox["painting"]["room"]) - 1] += 1
+    return votes.index(max(votes)) + 1
